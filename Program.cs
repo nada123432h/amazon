fdbfeaac3f402ddc290dcab3d86b8156e0d5cea1
nada123432 <nd.mostafa5@gmail.com>
2023-09-13 18:39:13 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int nSum = 0;
+            int nOdd = 0;
+            int nEven = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                nEven=(x%2==0) ? nEven += x : nOdd += x;
+
+
+                nSum += x;
+            }
+            Console.WriteLine(nSum);
+            Console.WriteLine(nOdd);
+            Console.WriteLine(nEven);
+
+        }
+    }
+}
